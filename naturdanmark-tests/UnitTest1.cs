ac282996b5394232a62f53cfbdb86d65d5d61db0
Evgeniy Gordienko <112853162+eudk@users.noreply.github.com>
2023-11-24 17:42:47 +0100

    Add project files.

---
+++ naturdanmark-tests/UnitTest1.cs
@@ -0,0 +1,21 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Firefox;
+using System.Collections.ObjectModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+//nuget pakker er installeret!!!
+
+namespace naturdanmark_tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
